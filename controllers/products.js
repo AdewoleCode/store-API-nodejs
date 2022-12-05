0f0d937edc3b2fa54e970f15d1755d65421049de
adewole <adeadewole1712@gmail.com>
2022-12-05 19:25:06 +0100

    using regex to dynaically pass in specific values to filter through our databse

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,11 @@
 const Products = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
+    const search = 't'
         // const products = await Products.find({ featured: false })
-        const products = await Products.find({ rating: 4 })
+        // const products = await Products.find({ rating: 4 })
+        const products = await Products.find({name: { $regex: search, $options: 'i' }, })
+
 
         res.status(200).json({ products })
         // console.log(products);
@@ -10,7 +13,7 @@ const getAllProductsStatic = async (req, res) => {
 
 
 const getAllProducts = async (req, res) => {
-    const { featured, company } = req.query
+    const { featured, company, name } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -21,7 +24,11 @@ const getAllProducts = async (req, res) => {
         queryObject.company = company
     }
 
-    console.log(queryObject, company);
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+
+    console.log(queryObject);
     const products = await Products.find(queryObject)
     res.status(200).json({ products})
 }
