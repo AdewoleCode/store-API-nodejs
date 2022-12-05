80d1a0c7b692b4565f74ef881d3220bdf6325ac4
adewole <adeadewole1712@gmail.com>
2022-12-06 00:56:17 +0100

    sorting my database dynamically with datas by extracting sort terms from the query string params in the url

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,11 @@
 const Products = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const search = 't'
+    // const search = 't'
         // const products = await Products.find({ featured: false })
         // const products = await Products.find({ rating: 4 })
-        const products = await Products.find({name: { $regex: search, $options: 'i' }, })
+        // const products = await Products.find({name: { $regex: search, $options: 'i' }, })
+        const products = await Products.find({}).sort('-price')
 
 
         res.status(200).json({ products })
@@ -13,7 +14,7 @@ const getAllProductsStatic = async (req, res) => {
 
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -28,8 +29,21 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: 'i' }
     }
 
-    console.log(queryObject);
-    const products = await Products.find(queryObject)
+    // console.log(queryObject);
+    let result = Products.find(queryObject)
+    if (sort) {
+
+        //seperating our value from the url with spaces to work with the sort method. it expects values seperated by space
+        const sortList = sort.split(',').join(' ')
+        //sort by whatever we/user/frontend/postman pass in the query sting params url
+        result = result.sort(sortList)
+        // console.log(sort);
+        // console.log(sortList);
+    } else {
+        //sort by what we pass in manually
+        result = result.sort('name')
+    }
+    const products = await result
     res.status(200).json({ products})
 }
 
--- populate.js
+++ populate.js
@@ -24,5 +24,4 @@ const start = async () => {
 }
 
 
-
 start()
