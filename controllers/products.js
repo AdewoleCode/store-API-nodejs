7e1531e3e6a97ae78d7a5f2c5744ddf963b9090b
adewole <adeadewole1712@gmail.com>
2022-12-07 02:18:28 +0100

    implementing a select, limit and skip functionality for the frontend on my databse

--- controllers/products.js
+++ controllers/products.js
@@ -14,7 +14,7 @@ const getAllProductsStatic = async (req, res) => {
 
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -43,6 +43,20 @@ const getAllProducts = async (req, res) => {
         //sort by what we pass in manually
         result = result.sort('name')
     }
+
+    if (fields) {
+        const FieldList = fields.split(',').join(' ')
+
+        result = result.select(FieldList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
+
+
     const products = await result
     res.status(200).json({ products})
 }
